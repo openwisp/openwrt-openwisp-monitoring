6e7507a4f89af3c938f7afd59c64893267c8386d
Federico Capoano <nemesis@ninux.org>
2018-02-16 15:19:19 +0100

    improved

--- netjson-monitoring.lua
+++ netjson-monitoring.lua
@@ -18,26 +18,32 @@ end
 
 -- collect wireless interface names
 status = ubus:call('network.wireless', 'status', {})
-netjson = {type='DeviceMonitoring', interfaces={}}
+netjson = {type='DeviceMonitoring'}
+interfaces = {}
 
 -- collect relevant wireless interface stats
 -- (traffic and connected clients)
 for radio_name, radio in pairs(status) do
     for i, interface in ipairs(radio.interfaces) do
         name = interface.ifname
-        clients = ubus:call('hostapd.'..name, 'get_clients', {})
-        rx_bytes = cat('/sys/class/net/wlan1/statistics/rx_bytes')
-        tx_bytes = cat('/sys/class/net/wlan1/statistics/tx_bytes')
-        table.insert(netjson.interfaces, {
-            name = name,
-            clients = clients.clients,
-            statistics = {
-                rx_bytes = tonumber(rx_bytes),
-                tx_bytes = tonumber(tx_bytes)
-            }
-        })
+        if name then
+            clients = ubus:call('hostapd.'..name, 'get_clients', {})
+            rx_bytes = cat('/sys/class/net/'..name..'/statistics/rx_bytes')
+            tx_bytes = cat('/sys/class/net/'..name..'/statistics/tx_bytes')
+            table.insert(interfaces, {
+                name = name,
+                clients = clients.clients,
+                statistics = {
+                    rx_bytes = tonumber(rx_bytes),
+                    tx_bytes = tonumber(tx_bytes)
+                }
+            })
+        end
     end
 end
 
-cjson = require('cjson')
+if next(interfaces) ~= nil then
+    netjson.interfaces = interfaces
+end
+
 print(cjson.encode(netjson))
