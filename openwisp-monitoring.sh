04fa85c52a3a5fa404d184331796befc7152ea8e
Federico Capoano <nemesis@ninux.org>
2018-02-19 16:59:53 +0100

    Allow send script to ignore certificate failure

--- openwisp-monitoring.sh
+++ openwisp-monitoring.sh
@@ -1,8 +1,16 @@
 uuid=$(uci get openwisp.http.uuid)
 key=$(uci get openwisp.http.key)
 base_url=$(uci get openwisp.http.url)
+verify_ssl=$(uci get openwisp.http.verify_ssl)
 url="$base_url/api/v1/monitoring/device/$uuid/?key=$key"
 data=$(/usr/sbin/netjson-monitoring)
+if [ "$verify_ssl" = 0 ]; then
+    curl_command='curl -k'
+else
+    curl_command='curl'
+fi
 # send data via POST
-curl -H "Content-Type: application/json" \
-     -X POST -d "$data" $url -I
+$($curl_command -H "Content-Type: application/json" \
+                -X POST \
+                -d "$data" \
+                -v $url)
