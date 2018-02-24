27a258cb193e0babd650587af86aba4cec0abab5
Federico Capoano <nemesis@ninux.org>
2018-02-24 17:34:54 +0100

    [netjson] Added iwinfo, general, resources

--- netjson-monitoring.lua
+++ netjson-monitoring.lua
@@ -11,31 +11,74 @@ function cat(file)
     return content
 end
 
+-- takes ubus wireless.status clients output and converts it to NetJSON
+function netjson_clients(clients)
+    local data = {}
+    for mac_address, properties in pairs(clients) do
+        properties.mac = mac_address
+        table.insert(data, properties)
+    end
+    return data
+end
+
 ubus = ubus_lib.connect()
 if not ubus then
     error('Failed to connect to ubusd')
 end
 
--- collect wireless interface names
-status = ubus:call('network.wireless', 'status', {})
-netjson = {type='DeviceMonitoring'}
+-- helpers
+iwinfo_modes = {
+    ['Master'] = 'access_point',
+    ['Client']= 'station',
+    ['Mesh Point'] = '802.11s',
+    ['Ad-Hoc'] = 'adhoc'
+}
+
+-- collect system info
+system_info = ubus:call('system', 'info', {})
+board = ubus:call('system', 'board', {})
+
+-- init netjson data structure
+netjson = {
+    type = 'DeviceMonitoring',
+    general = {
+        hostname = board.hostname,
+        local_time = system_info.localtime,
+        uptime = system_info.localtime
+    },
+    resources = {
+        load = system_info.load,
+        memory = system_info.memory,
+        swap = system_info.swap
+    }
+}
+
+-- collect device data
+wireless_status = ubus:call('network.wireless', 'status', {})
+network_status = ubus:call('network.device', 'status', {})
 interfaces = {}
 
 -- collect relevant wireless interface stats
 -- (traffic and connected clients)
-for radio_name, radio in pairs(status) do
+for radio_name, radio in pairs(wireless_status) do
     for i, interface in ipairs(radio.interfaces) do
         name = interface.ifname
         if name then
             clients = ubus:call('hostapd.'..name, 'get_clients', {})
-            rx_bytes = cat('/sys/class/net/'..name..'/statistics/rx_bytes')
-            tx_bytes = cat('/sys/class/net/'..name..'/statistics/tx_bytes')
+            iwinfo = ubus:call('iwinfo', 'info', {device = name})
             table.insert(interfaces, {
                 name = name,
-                clients = clients.clients,
-                statistics = {
-                    rx_bytes = tonumber(rx_bytes),
-                    tx_bytes = tonumber(tx_bytes)
+                statistics = network_status[name].statistics,
+                wireless = {
+                    ssid = iwinfo.ssid,
+                    mode = iwinfo_modes[iwinfo.mode] or iwinfo.mode,
+                    channel = iwinfo.channel,
+                    frequency = iwinfo.frequency,
+                    tx_power = iwinfo.txpower,
+                    signal = iwinfo.signal,
+                    noise = iwinfo.noise,
+                    country = iwinfo.country,
+                    clients = netjson_clients(clients.clients)
                 }
             })
         end
