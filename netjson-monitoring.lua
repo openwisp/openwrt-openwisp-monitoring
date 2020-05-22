91be85581bf970807db2b4fb9dcbad7e85fb9ce7
Federico Capoano <federico.capoano@gmail.com>
2020-05-22 12:57:50 -0500

    [feature] Added dhcp leases #2
    Closes #2

--- netjson-monitoring.lua
+++ netjson-monitoring.lua
@@ -4,6 +4,8 @@
 io = require('io')
 ubus_lib = require('ubus')
 cjson = require('cjson')
+uci = require('uci')
+uci_cursor = uci.cursor()
 
 -- split function
 function split(str, pat)
@@ -87,6 +89,43 @@ function get_neighbors()
    return arp_table
 end
 
+function parse_dhcp_lease_file(path, leases)
+    local f = io.open(path, 'r')
+    if not f then
+        return leases
+    end
+
+    for line in f:lines() do
+        local expiry, mac, ip, name, id = line:match('(%S+)%s+(%S+)%s+(%S+)%s+(%S+)%s+(%S+)')
+        table.insert(leases, {
+            expiry = tonumber(expiry),
+            mac_address = mac,
+            ip_address = ip,
+            client_name = name,
+            client_id = id
+         })
+    end
+
+    return leases
+end
+
+function get_dhcp_leases()
+    local uci_cursor = uci.cursor()
+    local dhcp_configs = uci_cursor:get_all('dhcp')
+    local leases = {}
+
+    if not dhcp_configs or not next(dhcp_configs) then
+        return nil
+    end
+
+    for name, config in pairs(dhcp_configs) do
+        if config and config['.type'] == 'dnsmasq' and config.leasefile then
+            leases = parse_dhcp_lease_file(config.leasefile, leases)
+        end
+    end
+    return leases
+end
+
 -- takes ubus wireless.status clients output and converts it to NetJSON
 function netjson_clients(clients)
     local data = {}
@@ -132,6 +171,7 @@ netjson = {
         memory = system_info.memory,
         swap = system_info.swap
     },
+    dhcp_leases = get_dhcp_leases(),
     neighbors = get_neighbors()
 }
 
