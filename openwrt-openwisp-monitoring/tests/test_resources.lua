d52fa2c84d8ebc38c3f56017f86d4ddfd8acabc7
Kapil Bansal <kapilbansal.gbpecdelhi@gmail.com>
2021-07-04 02:23:10 +0530

    [tests] Added neighbors test for netjson monitoring

--- openwrt-openwisp-monitoring/tests/test_resources.lua
+++ openwrt-openwisp-monitoring/tests/test_resources.lua
@@ -1,4 +1,4 @@
-package.path = package.path .. ";../files/lib/openwisp/?.lua"
+package.path = package.path .. ";../files/lib/openwisp/?.lua;../files/sbin/?.lua"
 
 local luaunit = require('luaunit')
 
@@ -8,6 +8,16 @@ TestResources = {
     setUp = function()
     local env = require('env1')
     package.loaded.io = env.io
+    end,
+    tearDown = function()
+    end
+}
+
+TestNetJSON = {
+    setUp = function()
+        local env = require('basic_env')
+        package.loaded.uci = env.uci
+        package.loaded.ubus = env.ubus
     end,
     tearDown = function()
     end
@@ -23,6 +33,38 @@ function TestResources.test_get_cpus()
     local resources = require('resources')
 
     luaunit.assertEquals(resources.get_cpus(), 8)
+end
+
+function TestNetJSON.test_resources()
+    local test_file_dir = './test_files/'
+    package.loaded.io = {
+        popen = function(arg)
+            local f = assert(io.tmpfile())
+            if arg == 'cat /proc/loadavg' then
+                f:write('0.37 0.95 1.23 2/873 56899\n')
+                f:seek('set',0)
+                return f
+            elseif arg == 'df' then
+                return io.open(test_file_dir .. 'disk_usage.txt')
+            elseif arg == 'cat /proc/cpuinfo | grep -c processor' then
+                f:write('8')
+                f:seek('set',0)
+                return f
+            else
+                f:write('')
+            end
+            f:seek('set',0)
+            return f
+        end,
+        open = function(arg)
+            return nil
+        end
+    }
+    local netjson = require('netjson-monitoring')
+    luaunit.assertNotNil(test_file_dir .. 'disk_usage.txt')
+    luaunit.assertNotNil(string.find(netjson, '"cpus":8'))
+    luaunit.assertNotNil(string.find(netjson, '"filesystem":"\\/dev\\/root"'))
+    luaunit.assertNotNil(string.find(netjson, '"used_percent":25'))
 end
 
 os.exit( luaunit.LuaUnit.run() )
