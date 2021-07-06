f960aaa10c346c116fa546f89fcc88f86699f304
Kapil Bansal <kapilbansal.gbpecdelhi@gmail.com>
2021-07-07 04:08:11 +0530

    [tests] Added wireless test data and tests for iwinfo clients

--- openwrt-openwisp-monitoring/tests/test_files/wireless_data.lua
+++ openwrt-openwisp-monitoring/tests/test_files/wireless_data.lua
@@ -2,60 +2,102 @@ local test_data = {}
 
 test_data.wireless_status={
   radio0 = {
+    up = true,
+    pending = false,
     autostart = true,
+    disabled = false,
+    retry_setup_failed = false,
     config = {
       channel = "11",
-      htmode = "HT20",
       hwmode = "11g",
-      path = "platform/10300000.wmac"
+      path = "1e140000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0",
+      htmode = "HT20",
+      log_level = 0,
+      disabled = false
     },
-    disabled = false,
-    interfaces = { {
+    interfaces = {
+      {
+        section = "wifi_mesh0",
+        ifname = "mesh0",
         config = {
-          encryption = "none",
-          mode = "ap",
-          network = { "lan" },
-          ssid = "OpenWrt"
+          ifname = "mesh0",
+          encryption = "psk2+ccmp",
+          key = "password",
+          mesh_id = "meshid",
+          mode = "mesh",
+          network = {"lan"}
         },
-        ifname = "wlan0-1",
-        section = "default_radio0"
-      }, {
-        config = {
-          encryption = "psk2",
-          key = "95535210",
-          mode = "sta",
-          network = { "wwan" },
-          ssid = "DIR-615-5A5B"
-        },
+        vlans = {},
+        stations = {}
+      },
+      {
+        section = "wifi_wlan0",
         ifname = "wlan0",
-        section = "wifinet2"
-      } },
-    pending = false,
-    retry_setup_failed = false,
-    up = true
+        config = {
+          ifname = "wlan0",
+          encryption = "psk2",
+          key = "password",
+          ssid = "ssid",
+          ieee80211r = true,
+          ft_over_ds = true,
+          ft_psk_generate_local = true,
+          rsn_preauth = true,
+          mode = "ap",
+          network = {"lan"}
+        },
+        vlans = {},
+        stations = {}
+      }
+    }
   },
   radio1 = {
-    autostart = true,
-    config = {
-      channel = "36",
-      htmode = "VHT80",
-      hwmode = "11a",
-      path = "pci0000:00/0000:00:00.0/0000:01:00.0"
-    },
-    disabled = false,
-    interfaces = { {
-        config = {
-          encryption = "none",
-          mode = "ap",
-          network = { "lan" },
-          ssid = "OpenWrt"
-        },
-        ifname = "wlan1",
-        section = "default_radio1"
-      } },
+    up = true,
     pending = false,
+    autostart = true,
+    disabled = false,
     retry_setup_failed = false,
-    up = true
+    config = {
+      hwmode = "11a",
+      path = "1e140000.pcie/pci0000:00/0000:00:01.0/0000:02:00.0",
+      htmode = "VHT80",
+      channel = "40",
+      log_level = 0,
+      disabled = false
+    },
+    interfaces = {
+      {
+        section = "wifi_mesh1",
+        ifname = "mesh1",
+        config = {
+          ifname = "mesh1",
+          encryption = "psk2+ccmp",
+          key = "password",
+          mesh_id = "meshID",
+          mode = "mesh",
+          network = {"lan"}
+        },
+        vlans = {},
+        stations = {}
+      },
+      {
+        section = "wifi_wlan1",
+        ifname = "wlan1",
+        config = {
+          ifname = "wlan1",
+          encryption = "psk2",
+          key = "password",
+          ssid = "ssid",
+          ieee80211r = true,
+          ft_over_ds = true,
+          ft_psk_generate_local = true,
+          rsn_preauth = true,
+          mode = "ap",
+          network = {"lan"}
+        },
+        vlans = {},
+        stations = {}
+      }
+    }
   }
 }
 
@@ -92,6 +134,200 @@ test_data.wlan1_clients = {
 
 test_data.wlan0_clients = {}
 
+test_data.mesh0_iwinfo = {
+  phy = "phy0",
+  ssid = "meshID",
+  bssid = "00:00:00:00:00:00",
+  country = "India",
+  mode = "Mesh Point",
+  channel = 11,
+  frequency = 2462,
+  frequency_offset = 0,
+  txpower = 20,
+  txpower_offset = 0,
+  quality = 43,
+  quality_max = 70,
+  signal = -67,
+  noise = 0,
+  bitrate = 6500,
+  encryption = {
+    enabled = true,
+    wpa = {3},
+    authentication = {"sae"},
+    ciphers = {"ccmp"}
+  },
+  htmodes = {
+    "HT20",
+    "HT40"
+  },
+  hwmodes = {
+    "b",
+    "g",
+    "n"
+  },
+  hwmode = "n",
+  htmode = "HT20",
+  hardware = {
+    id = {
+      5315,
+      30211,
+      5315,
+      30211
+    },
+    name = "MediaTek MT7603E"
+  }
+}
+
+test_data.mesh1_iwinfo = {
+  phy = "phy1",
+  ssid = "meshID",
+  bssid = "00:00:00:00:00:00",
+  country = "00",
+  mode = "Mesh Point",
+  channel = 40,
+  frequency = 5200,
+  frequency_offset = 0,
+  txpower = 20,
+  txpower_offset = 0,
+  quality = 34,
+  quality_max = 70,
+  signal = -76,
+  noise = -87,
+  bitrate = 195100,
+  encryption = {
+    enabled = true,
+    wpa = {3},
+    authentication = {"sae"},
+    ciphers = {"ccmp"}
+  },
+  htmodes = {
+    "HT20",
+    "HT40",
+    "VHT20",
+    "VHT40",
+    "VHT80",
+    "VHT80+80",
+    "VHT160"
+  },
+  hwmodes = {
+    "ac",
+    "n"
+  },
+  hwmode = "ac",
+  htmode = "VHT80",
+  hardware = {
+    id = {
+      5315,
+      30229,
+      30229,
+      5315
+    },
+    name = "MediaTek MT7615E"
+  }
+}
+
+test_data.mesh0_clients = {
+  results = {
+    {
+      mac = "00:00:00:00:00:00",
+      signal = -76,
+      signal_avg = -76,
+      noise = -87,
+      inactive = 0,
+      connected_time = 314439,
+      thr = 148687,
+      authorized = true,
+      authenticated = true,
+      preamble = "long",
+      wme = true,
+      mfp = true,
+      tdls = false,
+      ["mesh llid"] = 0,
+      ["mesh plid"] = 0,
+      ["mesh plink"] = "ESTAB",
+      ["mesh local PS"] = "ACTIVE",
+      ["mesh peer PS"] = "ACTIVE",
+      ["mesh non-peer PS"] = "ACTIVE",
+      rx = {
+        drop_misc = 610279,
+        packets = 40424675,
+        bytes = 280218466,
+        ht = false,
+        vht = true,
+        mhz = 80,
+        rate = 195000,
+        mcs = 1,
+        nss = 3,
+        short_gi = true
+      },
+      tx = {
+        failed = 2377,
+        retries = 11358818,
+        packets = 50325775,
+        bytes = 124429140,
+        ht = false,
+        vht = true,
+        mhz = 80,
+        rate = 195000,
+        mcs = 1,
+        nss = 3,
+        short_gi = true
+      }
+    }
+  }
+}
+
+test_data.mesh1_clients = {
+  results = {
+    {
+      mac = "00:00:00:00:00:00",
+      signal = -67,
+      signal_avg = -65,
+      noise = 0,
+      inactive = 6996,
+      connected_time = 314507,
+      thr = 4500,
+      authorized = true,
+      authenticated = true,
+      preamble = "long",
+      wme = true,
+      mfp = true,
+      tdls = false,
+      ["mesh llid"] = 0,
+      ["mesh plid"] = 0,
+      ["mesh plink"] = "ESTAB",
+      ["mesh local PS"] = "ACTIVE",
+      ["mesh peer PS"] = "ACTIVE",
+      ["mesh non-peer PS"] = "ACTIVE",
+      rx = {
+        drop_misc = 585666,
+        packets = 5533127,
+        bytes = 445412093,
+        ht = true,
+        vht = false,
+        mhz = 20,
+        rate = 6500,
+        mcs = 0,
+        ["40mhz"] = false,
+        short_gi = false
+      },
+      tx = {
+        failed = 0,
+        retries = 5,
+        packets = 12,
+        bytes = 1173,
+        ht = true,
+        vht = false,
+        mhz = 20,
+        rate = 6500,
+        mcs = 0,
+        ["40mhz"] = false,
+        short_gi = false
+      }
+    }
+  }
+}
+
 test_data.parsed_clients = {
     {
         aid=1,
@@ -123,6 +359,34 @@ test_data.parsed_clients = {
         wmm=true,
         wps=false
     }
+}
+
+test_data.mesh0_parsed_clients = {
+  {
+    auth=true,
+    authorized=true,
+    ht=false,
+    mac="00:00:00:00:00:00",
+    mfp=true,
+    noise=-87,
+    signal=-76,
+    vht=true,
+    wmm=true
+  }
+}
+
+test_data.mesh1_parsed_clients = {
+  {
+    auth=true,
+    authorized=true,
+    ht=true,
+    mac="00:00:00:00:00:00",
+    mfp=true,
+    noise=0,
+    signal=-67,
+    vht=false,
+    wmm=true
+  }
 }
 
 return test_data
--- openwrt-openwisp-monitoring/tests/test_wifi.lua
+++ openwrt-openwisp-monitoring/tests/test_wifi.lua
@@ -18,9 +18,26 @@ function TestWifi.test_parse_hostapd_clients()
     luaunit.assertEquals(wifi_functions.parse_hostapd_clients(wifi_data.wlan0_clients), {})
 end
 
+function TestWifi.test_parse_iwinfo_clients()
+    luaunit.assertEquals(
+        wifi_functions.parse_iwinfo_clients(wifi_data.mesh0_clients.results), wifi_data.mesh0_parsed_clients
+        )
+    luaunit.assertEquals(
+        wifi_functions.parse_iwinfo_clients(wifi_data.mesh1_clients.results), wifi_data.mesh1_parsed_clients
+        )
+end
+
 function TestWifi.test_netjson_clients()
+    -- testing hostapd clients
     luaunit.assertEquals(wifi_functions.netjson_clients(wifi_data.wlan1_clients, false), wifi_data.parsed_clients)
     luaunit.assertEquals(wifi_functions.netjson_clients(wifi_data.wlan0_clients, false), {})
+    -- testing iwinfo clients
+    luaunit.assertEquals(
+        wifi_functions.netjson_clients(wifi_data.mesh0_clients.results, true), wifi_data.mesh0_parsed_clients
+        )
+    luaunit.assertEquals(
+        wifi_functions.netjson_clients(wifi_data.mesh1_clients.results, true), wifi_data.mesh1_parsed_clients
+        )
 end
 
 
