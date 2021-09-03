04b7bf09d2710d0251dcd1b84872364f3936f949
Kapil Bansal <kapilbansal.gbpecdelhi@gmail.com>
2021-09-03 13:46:20 +0530

    [qa] Add shellcheck for checking scripts #55
    Closes #55

--- install-dev.sh
+++ install-dev.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 set -e
 apt-get update
-#install cmake and git
+# install cmake and git
 apt-get install -y cmake git
-#install lua
+# install shellcheck
+apt-get install shellcheck
+# install lua
 apt-get install -y lua5.1 liblua5.1-0-dev luarocks
-#install json-c
+# install json-c
 git clone https://github.com/json-c/json-c.git --depth=1
-cd json-c && cmake . && make install && cd ..  || { echo 'Installing json-c failed!' ; exit 1; }
+{ cd json-c && cmake . && make install && cd ..; } || { echo 'Installing json-c failed!' ; exit 1; }
 # install openwrt libubox and uci
 git clone https://git.openwrt.org/project/libubox.git --depth=1
-cd libubox && cmake . && make install && cd .. || { echo 'Installing libubox failed!' ; exit 1; }
+{ cd libubox && cmake . && make install && cd ..; } || { echo 'Installing libubox failed!' ; exit 1; }
 git clone https://git.openwrt.org/project/uci.git --depth=1
-cd uci && cmake . && make install && cd .. || { echo 'Installing uci failed!' ; exit 1; }
-#install nixio
+{ cd uci && cmake . && make install && cd ..; } || { echo 'Installing uci failed!' ; exit 1; }
+# install nixio
 luarocks install https://raw.githubusercontent.com/Neopallium/nixio/master/nixio-scm-0.rockspec
 # install luaformatter
 git clone --recurse-submodules https://github.com/Koihik/LuaFormatter.git
-cd LuaFormatter && cmake . && make install && cd .. || { echo 'Installing LuaFormatter failed'; exit 1; }
+{ cd LuaFormatter && cmake . && make install && cd ..; } || { echo 'Installing LuaFormatter failed'; exit 1; }
 # update links to shared libraries
 ldconfig -v
 # install luaunit
@@ -26,7 +28,7 @@ luarocks install luaunit
 luarocks install luacheck
 # install lua-cjson
 luarocks install lua-cjson
-#install luacov-coveralls
+# install luacov-coveralls
 luarocks install luacov-coveralls
-#clean
+# clean
 rm -rf json-c libubox uci LuaFormatter
