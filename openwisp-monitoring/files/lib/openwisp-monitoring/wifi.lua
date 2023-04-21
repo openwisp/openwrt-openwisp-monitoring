92014cdd8960f39a2bf72ea69a2e3b01c9ae4a37
Federico Capoano <f.capoano@openwisp.io>
2023-04-21 13:40:24 -0400

    [change] Improved mesh station information

--- openwisp-monitoring/files/lib/openwisp-monitoring/wifi.lua
+++ openwisp-monitoring/files/lib/openwisp-monitoring/wifi.lua
@@ -39,15 +39,27 @@ function wifi.parse_iwinfo_clients(clients)
   local data = {}
   for _, p in pairs(clients) do
     local client = {}
-    client.ht = p.rx.ht
     client.mac = p.mac
-    client.authorized = p.authorized
+    client.ht = p.rx.ht
     client.vht = p.rx.vht
+    client.he = p.rx.he
+    client.auth = p.authenticated
+    client.authorized = p.authorized
     client.wmm = p.wme
     client.mfp = p.mfp
-    client.auth = p.authenticated
     client.signal = p.signal
     client.noise = p.noise
+    client.signal_avg = p.signal_avg
+    client.mesh_llid = p['mesh llid']
+    client.mesh_plid = p['mesh plid']
+    client.mesh_plink = p['mesh plink']
+    client.mesh_local_ps = p['mesh local PS']
+    client.mesh_peer_ps = p['mesh peer PS']
+    client.mesh_non_peer_ps = p['mesh non-peer PS']
+    if p.thr then
+      -- collect expected throughput in bytes
+      client.throughput = p.thr * 1000
+    end
     table.insert(data, client)
   end
   return data
