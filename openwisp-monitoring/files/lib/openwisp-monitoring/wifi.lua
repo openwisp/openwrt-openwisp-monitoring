ce590b6b4157661096c910883e11d40cb202398f
Patrick Grimm <patrick@lunatiki.de>
2025-02-02 16:24:16 +0100

    add option for exclude reporting wifi client mac --ignore-wifi <interfaces>: Interfaces that ignored for wifi clients."  --ignore-wifi-mac <mac>: Space seperated list off Client MAC addresses that will be ignored.

--- openwisp-monitoring/files/lib/openwisp-monitoring/wifi.lua
+++ openwisp-monitoring/files/lib/openwisp-monitoring/wifi.lua
@@ -26,49 +26,53 @@ function wifi.invert_rx_tx(interface)
   return interface
 end
 
-function wifi.parse_hostapd_clients(clients)
+function wifi.parse_hostapd_clients(clients,exclude_mac)
   local data = {}
   for mac, properties in pairs(clients) do
-    properties.mac = mac
-    table.insert(data, properties)
+    if not exclude_mac[mac] then
+      properties.mac = mac
+      table.insert(data, properties)
+    end
   end
   return data
 end
 
-function wifi.parse_iwinfo_clients(clients)
+function wifi.parse_iwinfo_clients(clients,exclude_mac)
   local data = {}
   for _, p in pairs(clients) do
-    local client = {}
-    client.mac = p.mac
-    client.ht = p.rx.ht
-    client.vht = p.rx.vht
-    client.he = p.rx.he
-    client.auth = p.authenticated
-    client.authorized = p.authorized
-    client.wmm = p.wme
-    client.mfp = p.mfp
-    client.signal = p.signal
-    client.noise = p.noise
-    client.signal_avg = p.signal_avg
-    client.mesh_llid = p['mesh llid']
-    client.mesh_plid = p['mesh plid']
-    client.mesh_plink = p['mesh plink']
-    client.mesh_local_ps = p['mesh local PS']
-    client.mesh_peer_ps = p['mesh peer PS']
-    client.mesh_non_peer_ps = p['mesh non-peer PS']
-    if p.thr then
-      -- collect expected throughput in bytes
-      client.throughput = p.thr * 1000
+    if not exclude_mac[p.mac] then
+      local client = {}
+      client.mac = p.mac
+      client.ht = p.rx.ht
+      client.vht = p.rx.vht
+      client.he = p.rx.he
+      client.auth = p.authenticated
+      client.authorized = p.authorized
+      client.wmm = p.wme
+      client.mfp = p.mfp
+      client.signal = p.signal
+      client.noise = p.noise
+      client.signal_avg = p.signal_avg
+      client.mesh_llid = p['mesh llid']
+      client.mesh_plid = p['mesh plid']
+      client.mesh_plink = p['mesh plink']
+      client.mesh_local_ps = p['mesh local PS']
+      client.mesh_peer_ps = p['mesh peer PS']
+      client.mesh_non_peer_ps = p['mesh non-peer PS']
+      if p.thr then
+        -- collect expected throughput in bytes
+        client.throughput = p.thr * 1000
+      end
+      table.insert(data, client)
     end
-    table.insert(data, client)
   end
   return data
 end
 
 -- takes ubus wireless.status clients output and converts it to NetJSON
-function wifi.netjson_clients(clients, is_mesh)
-  return (is_mesh and wifi.parse_iwinfo_clients(clients) or
-           wifi.parse_hostapd_clients(clients))
-end
+function wifi.netjson_clients(clients, is_mesh, exclude_mac)
+  return (is_mesh and wifi.parse_iwinfo_clients(clients, exclude_mac) or
+           wifi.parse_hostapd_clients(clients, exclude_mac))
+end 
 
 return wifi
